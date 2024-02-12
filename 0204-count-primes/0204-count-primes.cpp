9f3407674fadafa2c289613d81d205c40dece252
Akshay Bangar <100552713+AkshayBangarAB@users.noreply.github.com>
2024-02-12 12:18:53 +0530

    Time: 522 ms (57.28%), Space: 11.5 MB (26.78%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -2,24 +2,24 @@ class Solution {
 public:
 
     int countPrimes(int n) {
-        if (n==0)
-        return 0;
 
+        if(n==0) return 0;
+        
         vector<bool> prime(n, true);
-        prime[0]= prime[1]= false;
+        prime[0] = prime[1]= false;
 
         int ans=0;
         for(int i=2; i<n; i++){
             if(prime[i]){
                 ans++;
 
-                int j= 2*i;
+                int j = 2*i;
                 while(j<n){
                     prime[j]= false;
-                    j+= i;
+                    j = j+ i;
                 }
             }
         }
-     return ans;
+        return ans;
     }
 };
